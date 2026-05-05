0d21e24ab5a4bc2a409bba570eff4bbe041d74cf
sampson <sampsongohengtze@gmail.com>
2026-05-05 16:27:21 +0800

    Clean up MDXWithAnimations to use only DynamicAnimation
    - Remove file-based animation imports (PendulumSim, etc.)
    - MDX now only uses DynamicAnimation for all animations
    - Animations should be saved to DB and accessed via <DynamicAnimation slug=... />
    - File-based animations can be migrated to DB later

--- components/MDXWithAnimations.tsx
+++ components/MDXWithAnimations.tsx
@@ -4,30 +4,6 @@ import dynamic from "next/dynamic";
 import type { MDXRemoteSerializeResult } from "next-mdx-remote";
 import type { ComponentType } from "react";
 
-const PendulumSim = dynamic(
-  () => import("@/components/animations/PendulumSim"),
-  {
-    ssr: false,
-    loading: () => null,
-  }
-);
-
-const DoublePendulumSim = dynamic(
-  () => import("@/components/animations/DoublePendulumSim"),
-  {
-    ssr: false,
-    loading: () => null,
-  }
-);
-
-const OrbitSim = dynamic(
-  () => import("@/components/animations/OrbitSim"),
-  {
-    ssr: false,
-    loading: () => null,
-  }
-);
-
 const DynamicAnimation = dynamic(
   () => import("./DynamicAnimation"),
   {
@@ -37,9 +13,6 @@ const DynamicAnimation = dynamic(
 );
 
 const mdxComponents = {
-  PendulumSim,
-  DoublePendulumSim,
-  OrbitSim,
   DynamicAnimation,
 };
 
