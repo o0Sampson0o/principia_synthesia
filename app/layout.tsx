6c59d20dad2c29f66cd7170185856469a937ce8f
sampson <sampsongohengtze@gmail.com>
2026-05-17 00:48:55 +0800

    fix(layout): suppress hydration warning on html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,7 @@ export default async function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <head>
         <style dangerouslySetInnerHTML={{ __html: themeStyle }} />
